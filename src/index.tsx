158ce8fc769c85a3bf72f74784fe385aec6df439
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-13 19:58:20 +0300

    Updates to Client Rendering APIs

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './redux/state';
+import {Provider} from "react-redux";
+import {store} from "./redux/state";
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
 );
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
+
