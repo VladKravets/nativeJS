0a4ae22a98ed6b5a3c355c4ec219fb8fcce0b160
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-18 15:16:17 +0300

    added keyAPI and small changes

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,11 +3,12 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key = '96696f0d';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+return axiosInstance.get(`/?t=${title}&apikey=96696f0d`)
     },
     searchFilmsByType: (title: string, type: string) => {
     }
